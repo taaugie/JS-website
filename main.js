375a39b122e428fc8588874cf0b91c236594c2cc
taaugie <taaugie@mac.com>
2020-04-23 14:29:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox-icon2.png');
+    if(mySrc === 'firefox-icon.png') {
+      myImage.setAttribute ('src','firefox-icon2.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','firefox-icon.png');
     }
 }
 let myButton = document.querySelector('button');
